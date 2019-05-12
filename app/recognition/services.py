58fbc9072de1c7f63d4076ba0a548bdbc8126c3d
Julia Samól <julia.samol@gmail.com>
2019-05-12 13:28:03 +0200

    Return error response if no ecodings have been found in samples

--- app/recognition/services.py
+++ app/recognition/services.py
@@ -1,9 +1,9 @@
-from typing import Iterator
+from typing import Iterator, List
 
 from app.recognition import recognition
 from app.recognition.models import Sample, BiometricPattern, MatchResult
 
 
-def match_sample(sample: Sample, patterns: Iterator[BiometricPattern]) -> MatchResult:
-    matched_user_id = recognition.test_sample(sample, patterns)
+def match_samples(samples: List[Sample], patterns: Iterator[BiometricPattern]) -> MatchResult:
+    matched_user_id = recognition.test_samples(samples, patterns)
     return MatchResult(matched_user_id)
